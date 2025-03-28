8136690b6afaf886139583acf542c91cec726c9c
carsonbeck <166659155+carsonbeck@users.noreply.github.com>
2025-03-28 13:11:33 -0400

    Add files via upload

---
+++ SDEV200-Module02-ProgrammingAssignment02/src/TestTriangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three sides of the triangle: ");
+        double side1 = input.nextDouble();
+        double side2 = input.nextDouble();
+        double side3 = input.nextDouble();
+
+        System.out.print("Enter the color of the triangle: ");
+        String color = input.next();
+
+        System.out.print("Is the triangle filled (true or false)? ");
+        boolean filled = input.nextBoolean();
+
+        Triangle triangle = new Triangle(side1, side2, side3);
+        triangle.setColor(color);
+        triangle.setFilled(filled);
+
+        System.out.println("\nTriangle Information:");
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+        System.out.println(triangle.toString());
+
+        input.close();
+    }
+}
